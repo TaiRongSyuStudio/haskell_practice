6409374523daf5be6928f09779b9d94b92fd5fce
Tai Rong Syu <s5952360@gmail.com>
2016-01-09 15:56:46 +0800

    Haskell leaning...
    Higher order functions

--- haskell_practice.hs
+++ haskell_practice.hs
@@ -32,6 +32,7 @@ circumference r = 2 * pi * r
 circumference' :: Double -> Double
 circumference' r = 2 * pi * r
 
+--Pattern matching
 factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
@@ -86,6 +87,7 @@ last' (x:[]) = x
 last' (x:y:[]) = y
 last' (x:y) = last' y
 
+--Guards
 bmiTell :: (RealFloat a) => a -> a -> String
 bmiTell weight height
         | weight / (height / 100) ^ 2 <= 18.5 = "You're underweight, you emo, you!"
@@ -104,6 +106,7 @@ a `myCompare` b
      | a == b    = EQ
      | otherwise = LT
 
+--Where
 bmiTell' :: (RealFloat a) => a -> a -> String
 bmiTell' weight height
         | bmi <= skinny = "You're underweight, you emo, you!"
@@ -115,5 +118,102 @@ bmiTell' weight height
               normal = 25.0
               fat    = 30.0
 
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ "." ++ [l] ++ "."
+        where (f:_) = firstname
+              (l:_) = lastname
 
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+        where bmi weight height = weight / (height / 100) ^ 2
 
+--Let
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h = 
+        let sideArea = 2 * pi * r * h;
+             topArea = pi * r ^ 2
+        in  sideArea + 2 * topArea
+
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / (h / 100) ^ 2, bmi >= 25.0]
+
+--Case
+head'' :: [a] -> a
+head'' xs = case xs of [] -> error "No head for empty lists!" 
+                       (x:_) -> x
+
+--Recursion
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+      | x > maxTail = x
+      | otherwise = maxTail
+      where maxTail = maximum' xs
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+      | n <= 0 = []
+      | otherwise = x : replicate' (n-1) x
+      
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _ 
+      | n <= 0 = []
+take' _ []     = []
+take' n (x:xs) = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+     | a == x = True
+     | otherwise = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smallerSorted = quicksort [a | a <- xs, a <= x];
+       biggerSorted = quicksort [a | a <- xs, a > x]
+  in smallerSorted ++ [x] ++ biggerSorted
+
+--Higher order functions
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred = compare 100 
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f y x = f x y 
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map f xs
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' f xs = [f x | x <- xs]
